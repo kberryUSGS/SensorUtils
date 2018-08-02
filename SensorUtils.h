e1944e81309f99eea3a8c824bc526608c51d8454
Tyler Wilson <twilson271828@gmail.com>
2018-08-01 18:20:20 -0700

    DO NOT MERGE...started code for the SensorUtils library.

---
+++ SensorUtils.h
@@ -0,0 +1,23 @@
+#ifndef SensorUtils_h
+#define SensorUtils_h
+
+
+#include <vector>
+#include <cmath>
+#include <cfloat>
+#include <climits>
+
+using namespace std;
+namespace csm {
+
+double PhaseAngle(vector<double> & instPosition, vector<double> & sunPosition);
+
+
+
+
+
+}
+
+
+
+#endif
