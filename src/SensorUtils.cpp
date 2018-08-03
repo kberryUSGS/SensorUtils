50b6e27b9aafe220952d003c27061b35d200b331
ihumphrey <ihumphrey@usgs.gov>
2018-08-03 11:29:00 -0700

    Add coveralls (#16)
    * Add coveralls to travis.yml
    
    * Modify CMakeLists.txt for coverage
    
    * Add foo() to SensorUtils.cpp for testing
    
    * add out-of-src build to travis.yml
    
    * removed foo() from SensorUtils.cpp
    
    * Only linux build will run coverage now
    
    * Changed from PUBLIC to PRIVATE for coverage options
    
    * Explicit -O0 for coverage
    
    * Remove unused testing method
    
    * Update .travis.yml

--- src/SensorUtils.cpp
+++ src/SensorUtils.cpp
@@ -5,7 +5,3 @@
 double PhaseAngle(vector<double>& instrumentPosition,vector<double>& sunPosition) {
     return 0.0;
 }
-
-double EmissionAngle(vector<double>& instrumentPosition,vector<double>& sunPosition) {
-    return 0.0;
-}
