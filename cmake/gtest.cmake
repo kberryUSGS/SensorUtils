3e1130890b8f5f0f81fdcb8eaaad9782259700af
Makayla Shepherd <mshepherd@usgs.gov>
2018-08-15 16:57:20 -0700

    Revert "Add gmock" (#141)
    * Revert "Revert "Update gtest.cmake (#139)" (#140)"
    
    This reverts commit cc0d7aaa7e73c39c6bd7241a67d39321169918f6.
    
    * Revert "Update gtest.cmake (#139)"
    
    This reverts commit 64af324b2893d03cb358a5365b819e3f368bd3c6.
    
    * Revert "version based on the build number (#137)"
    
    This reverts commit f7697c838855a91773eba99fb0da9f622c0408cf.
    
    * Revert "Add gmock (#138)"
    
    This reverts commit d63e17bc863a9fbccb8cc10e36c24e8ca34307d3.

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -4,14 +4,11 @@ if (NOT TARGET gtest)
   include_directories(SYSTEM
       ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}
       ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/include
-      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/../googlemock
-      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/../googlemock/include
       )
 
   set(GOOGLETEST_SOURCES
       ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest-all.cc
-      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/../googlemock/src/gmock-all.cc
-      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/../googlemock/src/gmock_main.cc
+      ${PROJECT_SOURCE_DIR}/${GOOGLETEST_ROOT}/src/gtest_main.cc
       )
 
   foreach(_source ${GOOGLETEST_SOURCES})
